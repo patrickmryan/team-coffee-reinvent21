2cc9eb35eba12457b320fa44c421c1450a676f85
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2021-11-29 14:58:02 -0800

    able to call lambda from API gateway

--- source/retrieve_solar_viability/retrieve_solar_viability.py
+++ source/retrieve_solar_viability/retrieve_solar_viability.py
@@ -7,6 +7,10 @@ import boto3
 
 def lambda_handler(event, context):
 
+    print(json.dumps(event))
+
+    qs_params = event['queryStringParameters']
+
     # API endpoint for lambda (read solrad_annual)
 # https://developer.nrel.gov/api/pvwatts/v6.json?api_key=7fP1f3Wg3ZdLeNiQue3LbTJ6Ust5c5C0secZ1jim&lat=40&lon=-105&system_capacity=1&azimuth=180&tilt=0&array_type=1&module_type=1&losses=10
 
@@ -14,8 +18,8 @@ def lambda_handler(event, context):
 
     params = {
         'api_key': api_key,
-        'lat' : event['latitude'],
-        'lon' : event['longitude'],
+        'lat' : qs_params['latitude'],
+        'lon' : qs_params['longitude'],
         'system_capacity': 2000,
         'azimuth': 90,
         'tilt': 0,
@@ -23,14 +27,28 @@ def lambda_handler(event, context):
         'module_type': 1,
         'losses': 10
     }
-
-    host = 'https://developer.nrel.gov/api/pvwatts/v6.json'
-    url = host + '?' + urlencode(params)
-
     http = urllib3.PoolManager(cert_reqs=ssl.CERT_NONE)
     urllib3.disable_warnings()
 
-    response = http.request( "GET", url )
-    json_resp = json.loads(response.data.decode())  # "utf-8"
+    host = 'https://developer.nrel.gov/api'
 
-    return json_resp
+    api = 'pvwatts/v6.json'
+    url = f'{host}/{api}?' + urlencode(params)
+
+    response = http.request( "GET", url )
+    solrad_annual = json.loads(response.data.decode())  # "utf-8"
+
+    api = 'utility_rates/v3.json'
+    url = f'{host}/{api}?' + urlencode(params)
+
+    response = http.request( "GET", url )
+    utility_rates = json.loads(response.data.decode())
+
+
+    return {
+        'dollar_per_kwh' : utility_rates['outputs']['residential'],
+
+
+        'solrad_annual' : solrad_annual,
+        'utility_rates': utility_rates
+    }
