869dbb136d390531f8c1d3930e84ec42f9d384a2
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2021-11-29 13:25:04 -0800

    able to hit API from lambda

--- source/retrieve_solar_viability/retrieve_solar_viability.py
+++ source/retrieve_solar_viability/retrieve_solar_viability.py
@@ -1,11 +1,36 @@
 import os, re
 import json
+from urllib.parse import urlencode
+import ssl, urllib3
 import boto3
 
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+
+    # API endpoint for lambda (read solrad_annual)
+# https://developer.nrel.gov/api/pvwatts/v6.json?api_key=7fP1f3Wg3ZdLeNiQue3LbTJ6Ust5c5C0secZ1jim&lat=40&lon=-105&system_capacity=1&azimuth=180&tilt=0&array_type=1&module_type=1&losses=10
+
+    api_key = os.environ.get('API_KEY')
+
+    params = {
+        'api_key': api_key,
+        'lat' : event['latitude'],
+        'lon' : event['longitude'],
+        'system_capacity': 2000,
+        'azimuth': 90,
+        'tilt': 0,
+        'array_type': 1,
+        'module_type': 1,
+        'losses': 10
     }
+
+    host = 'https://developer.nrel.gov/api/pvwatts/v6.json'
+    url = host + '?' + urlencode(params)
+
+    http = urllib3.PoolManager(cert_reqs=ssl.CERT_NONE)
+    urllib3.disable_warnings()
+
+    response = http.request( "GET", url )
+    json_resp = json.loads(response.data.decode())  # "utf-8"
+
+    return json_resp
