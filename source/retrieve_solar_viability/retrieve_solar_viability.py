1a22e11b31d3a62bde0d91c82066b1f7b4af9485
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2021-11-29 12:44:14 -0800

    added skeleton lambda function, along with cfn/sam template.

---
+++ source/retrieve_solar_viability/retrieve_solar_viability.py
@@ -0,0 +1,11 @@
+import os, re
+import json
+import boto3
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
