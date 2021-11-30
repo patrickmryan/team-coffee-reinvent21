cd8688a6c358bbd85264b672f25662796a45ca73
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2021-11-29 16:10:37 -0800

    support for all NREL parameters

--- source/retrieve_solar_viability/retrieve_solar_viability.py
+++ source/retrieve_solar_viability/retrieve_solar_viability.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
 
     print(json.dumps(event))
 
-    qs_params = event['queryStringParameters']
+    # qs_params = event['queryStringParameters']
 
     # API endpoint for lambda (read solrad_annual)
 # https://developer.nrel.gov/api/pvwatts/v6.json?api_key=7fP1f3Wg3ZdLeNiQue3LbTJ6Ust5c5C0secZ1jim&lat=40&lon=-105&system_capacity=1&azimuth=180&tilt=0&array_type=1&module_type=1&losses=10
@@ -18,8 +18,6 @@ def lambda_handler(event, context):
 
     params = {
         'api_key': api_key,
-        'lat' : qs_params['latitude'],
-        'lon' : qs_params['longitude'],
         'system_capacity': 2000,
         'azimuth': 90,
         'tilt': 0,
@@ -27,6 +25,10 @@ def lambda_handler(event, context):
         'module_type': 1,
         'losses': 10
     }
+
+    for key, value in (event['queryStringParameters']).items():
+        params[key] = value
+
     http = urllib3.PoolManager(cert_reqs=ssl.CERT_NONE)
     urllib3.disable_warnings()
 
@@ -35,8 +37,12 @@ def lambda_handler(event, context):
     api = 'pvwatts/v6.json'
     url = f'{host}/{api}?' + urlencode(params)
 
-    response = http.request( "GET", url )
+    print(url)
+    print(json.dumps(params))
+
+    response = http.request( "GET", url)
     details = json.loads(response.data.decode())  # "utf-8"
+    print(json.dumps(details))
 
     solrad_annual = details['outputs']['solrad_annual']
 
